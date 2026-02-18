503acd5c7d9f2f15fe7d54dc5808e9f2ee2f2387
Serge Kotchourko <serge.kotchourko@gmail.com>
2026-02-18 20:53:47 +0100

    ref: changed to nerdfonts

--- weather.py
+++ weather.py
@@ -1,23 +1,23 @@
 import configparser
-import requests
-
 import sys
+
+import requests
 
 # Read config file
 config = configparser.ConfigParser()
 config.read(f"{sys.argv[1]}/config.ini")
 
 dict_weather_group_icon = {
-    200: "",
-    300: "",
-    500: "",
-    600: "",
-    700: "",
-    800: ["", ""],
-    801: ["", ""],
-    802: "",
-    803: "",
-    804: "",
+    200: "\ue31d",
+    300: "\ue316",
+    500: "\ue318",
+    600: "\ue31a",
+    700: "\ue313",
+    800: ["\uf522", "\uf4ee"],
+    801: ["\ue302", "\ue32e"],
+    802: "\ue33d",
+    803: "\ue33d",
+    804: "\ue33d",
 }
 
 URL = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units={}&exclude=minutely,hourly,daily,alerts&appid={}"
@@ -32,22 +32,24 @@ url_formatted = URL.format(
 try:
     current = requests.get(url_formatted).json()
 except:
-    print("")
+    print("\udb80\udd64")
     exit()
 
 if current["cod"] != 200:
-    print("")
+    print("\udb80\udd64")
     exit()
 
 # get weather icon
 weather_id = current["weather"][0]["id"]
 weather_icon = dict_weather_group_icon.get(weather_id, (weather_id // 100) * 100)
 if isinstance(weather_icon, int):
-    weather_icon = dict_weather_group_icon.get((weather_id // 100) * 100, 'ERROR')
+    weather_icon = dict_weather_group_icon.get((weather_id // 100) * 100, "ERROR")
 
 if isinstance(weather_icon, list):
     weather_icon = (
-        weather_icon[0] if current["sys"]["sunrise"] < current["dt"] < current["sys"]["sunset"] else weather_icon[1]
+        weather_icon[0]
+        if current["sys"]["sunrise"] < current["dt"] < current["sys"]["sunset"]
+        else weather_icon[1]
     )
 
 # get unit
